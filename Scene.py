8f5a5cb79ddc1ebc43f9d97734db0a457bc4786e
qcamiloq <qcamiloq@gmail.com>
2016-05-01 13:19:04 -0500

    Label, comboBox para guardar y cargar, label, lineEdit y boton, todo funcional listo para insertar codigo

--- Scene.py
+++ Scene.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 from __main__ import vtk, qt, ctk, slicer
 
 
@@ -42,21 +43,47 @@ class SceneWidget:
 
     # Layout within the sample collapsible button
     sampleFormLayout = qt.QFormLayout(sampleCollapsibleButton)
-
-    ########################
-    # Add tutorial code here
-    ########################
-    
-    # Add vertical spacer
     self.layout.addStretch(1)
+    labelSeleccionCargarGuardar = qt.QLabel("Desea cargar o guardar? ") #Se crea label para seleccion de tornillo a manipular
+    sampleFormLayout.addWidget(labelSeleccionCargarGuardar) #Se añade label
+ 
+    self.comboBoxSeleccionCargarGuardar = qt.QComboBox() #Se crea comboBox para seleccionar tornillo
+    self.comboBoxSeleccionCargarGuardar.addItem("Cargar Scene") #Se añade opciones
+    self.comboBoxSeleccionCargarGuardar.addItem("Guardar Scene")
+    sampleFormLayout.addWidget(self.comboBoxSeleccionCargarGuardar)
 
-    # Set local var as instance attribute
-    self.helloWorldButton = helloWorldButton
 
-  def onHelloWorldButtonClicked(self):
-    print "Hello World !"
 
-    ########################
-    # Add tutorial code here
-    ########################
+    labelNombreScene = qt.QLabel("Nombre: ") #Se crea label para seleccion de tornillo a manipular
+    sampleFormLayout.addWidget(labelNombreScene)
+
+    self.nombreSceceTextEdit = qt.QLineEdit()
+   
+    self.nombreSceceTextEdit.textChanged.connect(self.textchanged1)
+    sampleFormLayout.addWidget(self.nombreSceceTextEdit)
+
+    self.cargarGuardarButton = qt.QPushButton("Cargar/Guardar Scene") #Se crea boton pulsable, con texto "Apply"
+    sampleFormLayout.addWidget(self.cargarGuardarButton) #Se añade el boton al layout del boton colapsable
+    self.cargarGuardarButton.connect('clicked(bool)',self.onApply3)
+
+
+
+  def textchanged1(self,text):
+      self.name = text
+
+  def onApply3(self):
+    if self.comboBoxSeleccionCargarGuardar.currentIndex == 0:
+        try:
+            print ("Cargaste: " + self.name)
+        except(AttributeError):
+            qt.QMessageBox.critical(slicer.util.mainWindow(),'Error cargar/guardar', 'Espacio en blanco')
+
+
+    else:
+        try:
+            print ("Gurdaste: " + self.name)
+            
+        except(AttributeError):
+            qt.QMessageBox.critical(slicer.util.mainWindow(),'Error cargar/guardar', 'Espacio en blanco')
+    
 
